52e17266c15c04e663fba353e6ce8a6caa008f13
Alexander <Mupuk55@web.de>
2017-07-25 02:21:57 +0200

    155 Tank Moves

--- Source/BattleTank/Private/TankTrack.cpp
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -3,4 +3,9 @@
 void UTankTrack::SetThrottle(float Throttle)
 {
 	UE_LOG(LogTemp, Warning, TEXT("%f"), Throttle);
+
+	FVector ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	FVector ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
--- Source/BattleTank/Public/TankTrack.h
+++ Source/BattleTank/Public/TankTrack.h
@@ -13,4 +13,6 @@ public:
 	UFUNCTION (BlueprintCallable, Category = Input) void SetThrottle(float Throttle);
 
 private:
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = Setup) float TrackMaxDrivingForce = 500000;
 };
